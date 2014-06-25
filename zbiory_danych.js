c55386333e483f3f4b34ef25fe25f87bb7f4875e
Michal <michal@szkodzinski.pl>
2014-06-25 14:12:32 +0200

    style, poprawki

--- zbiory_danych.js
+++ zbiory_danych.js
@@ -1,28 +1,30 @@
 function losuj() {
     // tresc
     var arr = []
-    while (arr.length < 3) {
-        var randomnumber = Math.ceil(Math.random() * $zbiory.length)
-        var found = false;
+    while (arr.length < 2) {
+        var randomnumber = Math.ceil(Math.random() * $zbiory.length),
+            found = false;
         for (var i = 0; i < arr.length; i++) {
-            if (arr[i] == randomnumber)
+            if (arr[i] == randomnumber) {
                 found = true;
-            break
+            }
+            break;
         }
-        if (!found)
-            arr[arr.length] = randomnumber;
+        if (!found) {
+            arr[arr.length] = randomnumber % $zbiory.length;
+        }
     }
 
     // polozenie
     $('.dataset').each(function (idx, el) {
         jel = $(el);
         jel.text(arr[idx] + '. ' + $zbiory[arr[idx]]);
-        jel.css('left', Math.random() * 70 + '%');
-        jel.css('top', 200 + Math.random() * 80  + idx * 200 + 'px');
+//        jel.css('left', Math.random() * 70 + '%');
+//        jel.css('top', 350 + Math.random() * 80  + idx * 200 + 'px');
     });
 
-    targetNo = Math.ceil(Math.random() * target.length);
-    $('.target-group span').text(target[targetNo]);
+    targetNo = Math.ceil(Math.random() * target.length) % target.length;
+    $('.target-group span').text('' + targetNo + '. ' + target[targetNo]);
 }
 
 $(function () {
